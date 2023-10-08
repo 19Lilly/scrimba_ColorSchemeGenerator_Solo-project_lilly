8af56e14915902ad284a54860d94a37e2ccfa44b
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2023-10-08 22:42:51 +0200

    add quad palette, wrong spelling corrected

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let colorInput = document.querySelector(".pick-color-input");
-let palleteInput = document.querySelector(".pallete-type");
+let paletteInput = document.querySelector(".palette-type");
 const getColorSchemeBtn = document.querySelector(".get-color-scheme");
-const colorPalleteEl = document.querySelector(".color-pallete");
+const colorPaletteEl = document.querySelector(".color-palette");
 let fetchHexCode = [];
 
 //chatch the user selection
@@ -10,14 +10,14 @@ colorInput.addEventListener("change", function (e) {
   colorInput = e.target;
 });
 
-palleteInput.addEventListener("change", function (e) {
-  palleteInput = e.target;
+paletteInput.addEventListener("change", function (e) {
+  paletteInput = e.target;
 });
 
 //fetch color palete from API based on user input and render it on page
 getColorSchemeBtn.addEventListener("click", function () {
   let hex = colorInput.value.slice(1);
-  let mode = palleteInput.value.toLowerCase();
+  let mode = paletteInput.value;
   let url = `https://www.thecolorapi.com/scheme?hex=${hex}&mode=${mode}&count=6`;
 
   const options = {
@@ -39,6 +39,6 @@ getColorSchemeBtn.addEventListener("click", function () {
             <p class="hex-code">${color.hex.value}</p>
         </div>`;
       });
-      colorPalleteEl.innerHTML = html;
+      colorPaletteEl.innerHTML = html;
     });
 });
